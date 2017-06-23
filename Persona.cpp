6f0adc331438d2122bf110841c8fda6becf5cffe
Jose Rojas <jose23rojas@unitec.edu>
2017-06-23 13:28:44 -0600

    clase Persona TERMINADA

---
+++ Persona.cpp
@@ -0,0 +1,24 @@
+// Persona.cpp
+
+#include "Persona.h"
+
+Persona::Persona() { }
+
+Persona::Persona(string nombre, int edad, string numeroID)
+{
+	this -> nombre = nombre;
+	this -> edad = edad;
+	this -> numeroID = numeroID;
+}
+
+// nombre
+void Persona::setNombre(string nombre) { this -> nombre = nombre; }
+string Persona::getNombre() { return nombre; }
+
+// edad
+void Persona::setEdad(int edad) { this -> edad = edad; }
+int Persona::getEdad() { return edad; }
+
+// numeroID
+void Persona::setNumeroID(string numeroID) { this -> numeroID = numeroID; }
+string Persona::getNumeroID() { return numeroID; }
---
+++ Persona.h
@@ -0,0 +1,34 @@
+// Persona.h
+
+#include <string>
+
+using namespace std;
+
+#ifndef PERSONA_H
+#define PERSONA_H
+
+class Persona
+{
+	protected:
+		string nombre;
+		int edad;
+		string numeroID; // solamente lleva cuatro numeros
+
+	public:
+		Persona();
+		Persona(string, int, string); // nombre, edad, numeroID
+
+		// nombre
+		void setNombre(string);
+		string getNombre();
+
+		// edad
+		void setEdad(int);
+		int getEdad();
+
+		// numeroID
+		void setNumeroID(string);
+		string getNumeroID();
+};
+
+#endif
